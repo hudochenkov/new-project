a9d245a9c1a9291a16631e9ac71af1cfb8f59dc1
Aleks Hudochenkov <aleks@hudochenkov.com>
2016-04-04 01:19:43 +0300

    Migrate to Gulp 4

--- README.md
+++ README.md
@@ -1,19 +1,15 @@
 # How to work
 
-All files for production are compiled with [Grunt]. CSS compiled with [PostCSS]. JavaScript libs and plugins are concatenated to the single file. Images are optimized and SVG compiled to SVG-sprite. HTML just copied.
+All files for production are compiled with [Gulp]. CSS compiled with [PostCSS]. JavaScript libs and plugins are concatenated to the single file. Images are optimized. HTML just copied.
 
 ## Setup
 
 1. Install [Node.js].
-2. Install [Grunt] CLI:
-
-		$ npm install -g grunt-cli
-
-3. Install development modules:
+2. Install development modules:
 
 		$ npm install
 
-4. Run `grunt build`.
+3. Run `npm run gulp build`.
 
 ## Folders structure
 
@@ -25,7 +21,6 @@ All files for production are compiled with [Grunt]. CSS compiled with [PostCSS].
 
 * dev root — HTML.
 * `img` — images.
-	* `img/svg-sprite` — SVGs for SVG-sprite.
 	* `img/temp` — temporary images. They don't go to production and for demonstration purpose only.
 * `js` — JavaScript.
 	* `js/libs` — JavaScript libraries and plugins, that can't be installed via npm.
@@ -35,24 +30,18 @@ All files for production are compiled with [Grunt]. CSS compiled with [PostCSS].
 
 Start watching service which generates _dev_ version on each source file change, also this start local webserver with autoreload:
 
-	$ grunt
+	$ npm run gulp
 
 Generate _production_ (minified and optimized) version:
 
-	$ grunt build
+	$ npm run gulp build
 
 ## Other Grunt-tasks
 
-**lint** — check code style in stylesheets and in JavaScript.
-
-**compress** — make .zip:
-
-* **all** — sources and compiled files.
-* **markup** — only compiled
-* **source** — only sources
+**lint** — check code style in stylesheets.
 
 **deploy** — upload files to the server.
 
-[Grunt]: http://gruntjs.com/
+[Gulp]: https://github.com/gulpjs/gulp/tree/4.0
 [PostCSS]: https://github.com/postcss/postcss/
 [Node.js]: https://nodejs.org/
---
+++ gulpfile.js
@@ -0,0 +1,298 @@
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var postcss = require('gulp-postcss');
+var browserSync = require('browser-sync');
+var del = require('del');
+var ftp = require('vinyl-ftp');
+var gutil = require('gulp-util');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var header = require('gulp-header');
+var cssnano = require('cssnano');
+var stylelint = require('stylelint');
+var reporter = require("postcss-reporter");
+var fs = require('fs');
+
+var project = {
+	src: 'dev',
+	build: 'build',
+	css: {
+		src: 'dev/pcss/main.pcss',
+		build: 'main.css',
+		dir: 'dev/pcss/'
+	},
+	js: {
+		src: 'dev/js',
+		build: 'build/js'
+	},
+	img: {
+		src: 'dev/img',
+		build: 'build/img',
+		allExtensions: '**/*.{png,jpg,gif,svg}',
+		svgSprite: {
+			src: 'svg-sprite/*.svg',
+			build: 'build/img/sprite.svg'
+		}
+	}
+};
+
+var pkg = require('./package.json');
+
+gulp.task('clean', function() {
+	return del([project.build + '/**/*'], {
+		dot: true
+	});
+});
+
+var processors = [
+	require('postcss-import')(),
+	require('postcss-mixins')(),
+	require('postcss-nested')(),
+	require('postcss-simple-vars')(),
+	require('postcss-property-lookup')(),
+	require('postcss-assets')({
+		basePath: 'dev'
+	}),
+	require('postcss-inline-svg')({
+		path: 'dev'
+	}),
+	require('postcss-calc')(),
+	require('postcss-hexrgba')(),
+	require('postcss-custom-media')(),
+	require('postcss-media-minmax')(),
+	require('lost')(),
+	require('autoprefixer')({
+		browsers: ['last 2 versions', '> 1%', 'Android >= 4', 'iOS >= 8']
+	})
+];
+
+gulp.task('styles:default', function() {
+	return gulp.src(project.css.src)
+		.pipe(plumber())
+		.pipe(postcss(processors))
+		.pipe(rename(project.css.build))
+		.pipe(gulp.dest(project.build))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('styles:minify', function() {
+	return gulp.src(project.build + '/' + project.css.build)
+		.pipe(plumber())
+		.pipe(postcss([
+			cssnano({
+				autoprefixer: false,
+				calc: false,
+				colormin: true,
+				convertValues: false,
+				discardComments: true,
+				discardDuplicates: true,
+				discardEmpty: true,
+				discardUnused: true,
+				mergeIdents: true,
+				mergeLonghand: true,
+				mergeRules: false,
+				minifyFontValues: true,
+				minifyGradients: true,
+				minifySelectors: true,
+				normalizeCharset: true,
+				normalizeUrl: false,
+				orderedValues: false,
+				reduceIdents: true,
+				uniqueSelectors: true,
+				zindex: true
+			})
+		]))
+		.pipe(gulp.dest(project.build));
+});
+
+gulp.task('styles:lint', function() {
+	return gulp.src(project.css.dir + '*.pcss')
+		.pipe(postcss([
+			stylelint(),
+			reporter({
+				clearMessages: true
+			})
+		]));
+});
+
+gulp.task('styles',
+	gulp.series(
+		'styles:default',
+		'styles:minify'
+	)
+);
+
+gulp.task('copy:images', function() {
+	return gulp.src(
+			[
+				project.img.src + '/' + project.img.allExtensions,
+				'!' + project.img.src + '/' + project.img.svgSprite.src
+			],
+			{
+				since: gulp.lastRun('copy:images')
+			}
+		)
+		.pipe(gulp.dest(project.img.build))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('copy:html', function() {
+	return gulp.src(project.src + '/*.html', { since: gulp.lastRun('copy:html') })
+		.pipe(gulp.dest(project.build))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('copy:js', function() {
+	return gulp.src(project.js.src + '/*.js', { since: gulp.lastRun('copy:html') })
+		.pipe(gulp.dest(project.js.build))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('copy',
+	gulp.parallel(
+		'copy:images',
+		'copy:html',
+		'copy:js'
+	)
+);
+
+gulp.task('concat', function() {
+	return gulp.src([
+			'node_modules/jquery/dist/jquery.min.js',
+			project.js.src + '/libs/*.js'
+		])
+		.pipe(concat('libs.js'))
+		.pipe(gulp.dest(project.js.build));
+});
+
+gulp.task('banner', function() {
+	return gulp.src(project.build + '/' + project.css.build)
+		.pipe(header(
+			'/*\n' +
+			'Author:     Aleks Hudochenkov (hudochenkov.com)\n' +
+			'Version:    <%= date %>\n' +
+			'-----------------------------------------------------------------------------*/\n',
+			{
+				date: new Date().toJSON().slice(0,10).split('-').reverse().join('.')
+			}
+		))
+		.pipe(gulp.dest(project.build));
+});
+
+gulp.task('upload', function() {
+	function isFileExist(filePath) {
+		try {
+			fs.statSync(filePath);
+		} catch(err) {
+			if (err.code == 'ENOENT') return false;
+		}
+
+		return true;
+	}
+
+	var secret = isFileExist(process.env.HOME + '/.gulp-ftp.json') ? require(process.env.HOME + '/.gulp-ftp.json') : {};
+
+	var connection = ftp.create({
+		host: secret.host,
+		port: secret.port,
+		user: secret.username,
+		password: secret.password,
+		parallel: 8,
+		log: gutil.log
+	});
+
+	var url = 'http://hudochenkov.com/show/' + pkg.name + '/';
+
+	console.log('URL: ' + url);
+
+	// Copy URL to clipboard
+	var proc = require('child_process').spawn('pbcopy');
+	proc.stdin.write(url);
+	proc.stdin.end();
+
+	return gulp.src(
+			[project.build + '/**'],
+			{
+				base: './' + project.build,
+				buffer: false
+			}
+		)
+		.pipe(connection.newer('/show/' + pkg.name))
+		.pipe(connection.dest('/show/' + pkg.name));
+});
+
+gulp.task('server', function() {
+	browserSync.init({
+		server: {
+			baseDir: project.build
+		},
+		notify: false,
+		online: false,
+		ghostMode: false
+	});
+});
+
+gulp.task('watch', function() {
+	gulp.watch([
+		project.css.dir + '/*.pcss'
+	], gulp.series('styles:default'));
+
+	gulp.watch([
+		project.img.src + '/' + project.img.allExtensions,
+		'!' + project.img.src + '/' + project.img.svgSprite.src
+	], gulp.series('copy:images'));
+
+	gulp.watch([
+		project.js.src + '/libs/*.js'
+	], gulp.series('concat'));
+
+	gulp.watch([
+		project.js.src + '/*.js'
+	], gulp.series('copy:js'));
+
+	gulp.watch([
+		project.src + '/*.html'
+	], gulp.series('copy:html'));
+});
+
+gulp.task('default',
+	gulp.series(
+		gulp.parallel(
+			'styles:default',
+			'copy',
+			'concat'
+		),
+		gulp.parallel(
+			'server',
+			'watch'
+		)
+	)
+);
+
+gulp.task('build',
+	gulp.series(
+		'clean',
+		gulp.parallel(
+			gulp.series(
+				'styles:default',
+				'styles:minify',
+				'banner'
+			),
+			'concat',
+			'copy'
+		)
+	)
+);
+
+gulp.task('lint',
+	gulp.parallel(
+		'styles:lint'
+	)
+);
+
+gulp.task('deploy',
+	gulp.series(
+		'build',
+		'upload'
+	)
+);
